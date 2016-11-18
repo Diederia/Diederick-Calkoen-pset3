12e41c9adbfc51b3dde1ebac1488180f5da5eee1
Diederia <d.w.calkoen@student.vu.nl>
2016-11-18 15:44:25 +0100

    komt celview nu wel mee

--- Diederick-Calkoen-pset3/CellViewController.swift
+++ Diederick-Calkoen-pset3/CellViewController.swift
@@ -1,14 +1,14 @@
 //
-//  SingleViewController.swift
+//  CellViewController.swift
 //  Diederick-Calkoen-pset3
 //
-//  Created by Diederick Calkoen on 15/11/16.
+//  Created by Diederick Calkoen on 16/11/16.
 //  Copyright © 2016 Diederick Calkoen. All rights reserved.
 //
 
 import UIKit
 
-class SingleViewController: UIViewController {
+class CellViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var yearLabel: UILabel!
@@ -16,18 +16,17 @@ class SingleViewController: UIViewController {
     @IBOutlet weak var actorsLabel: UILabel!
     @IBOutlet weak var bannerImage: UIImageView!
     @IBOutlet weak var infoText: UITextView!
-    @IBOutlet weak var saveButton: UIButton!
-    
+        
     var movieTitle: String?
     var movieYear: String?
     var movieGenre: String?
     var movieActors: String?
     var movieBanner: String?
     var movieInfo: String?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         titleLabel.text = movieTitle
         yearLabel.text = movieYear
@@ -35,26 +34,31 @@ class SingleViewController: UIViewController {
         actorsLabel.text = movieActors
         infoText.text = movieInfo
         
-        // How to get the image viewed
-        let strImage : UIImage=arrDict[indexPath.row] .valueForKey("image") as! UIImage
-
-        
+    
+        if (movieBanner != "N/A") {
+            let banner = movieBanner!
+            let urlBanner = NSURL(string: banner)
+            let dataBanner = NSData(contentsOf: urlBanner! as URL)
+            bannerImage.image = UIImage(data: dataBanner as! Data)
+        }
+        else {
+            bannerImage.image = UIImage(named: "no-image")
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+        /*         // MARK: - Navigation
+     
+        // In a storyboard-based application, you will often want to do a little preparation before navigation
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-    }
-    */
-
+        }
+        */
+        
 }
